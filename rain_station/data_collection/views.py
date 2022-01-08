0011c9c37924772758b4f8e8792746c406fe7f2a
Orin <orin29377053@gmail.com>
2022-01-08 22:39:34 +0800

    test gitignore

--- rain_station/data_collection/views.py
+++ rain_station/data_collection/views.py
@@ -3,6 +3,6 @@ from datetime import datetime
 # Create your views here.
 from django.http import HttpResponse
 def hello_world(request):
-     return render(request, 'hello_world.html', {
+     return render(request, 'index.html', {
         'current_time': str(datetime.now()),
     })
